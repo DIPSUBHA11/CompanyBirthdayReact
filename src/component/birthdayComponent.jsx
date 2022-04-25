c3da7c0b20f7f807536869e0a510da7695980065
DIPSUBHA11 <37392524+DIPSUBHA11@users.noreply.github.com>
2022-04-26 00:39:59 +0530

    remove s3 add mutlet

--- src/component/birthdayComponent.jsx
+++ src/component/birthdayComponent.jsx
@@ -1,9 +1,10 @@
 import React, { useRef, useState } from 'react'
 
 const defaultState = {
-    employeeid:'',
+    employeeid: '',
     employeename: '',
-    filename: ''
+    filename: '',
+    employeeBirthday:''
 }
 
 const BirthDayForm = () => {
@@ -19,29 +20,11 @@ const BirthDayForm = () => {
         const data = new FormData();
         data.append('ImageFile', fileInputRef.current.files[0])
         data.append('name', employeeDetails.employeename)
-        const file = fileInputRef.current.files[0];
-        const { url } = await fetch('http://localhost:4104/s3Url').then(res => res.json())
-        console.log(url)
-        await fetch(url, {
-            method: 'put',
-            headers: {
-                "Content-Type": "multipart/form-data"
-            },
-            body: file
-        })
-
-        const imageURl = url.split('?')[0]
-        const sendEmployeeDetails= {
-            id:employeeDetails.employeeid,
-            name: employeeDetails.employeename,
-            imageUrl: imageURl
-        }
+        data.append('id', employeeDetails.employeeid)
+        data.append('birthday',employeeDetails.employeeBirthday)
         await fetch('http://localhost:4104/upload', {
             method: 'POST',
-            body: JSON.stringify(sendEmployeeDetails),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
+            body: data
         }).
             then(res => {
                 if (res.status === 200)
@@ -53,12 +36,14 @@ const BirthDayForm = () => {
             <form method="post" onSubmit={handleSubmit} encType="multipart/form-data">
                 <h1>Enter Employee details</h1>
                 <div className="field">
-                <label>Id No-:</label>
+                    <label>Id No-:</label>
                     <input type="text" id="id_name" name="employeeid" autoComplete="off" placeholder="Enter the id" value={employeeDetails.employeeid} onChange={handleinput} />
                     <small></small>
                     <label>Name:</label>
                     <input type="text" id="fname" name="employeename" autoComplete="off" placeholder="Enter the name" value={employeeDetails.employeename} onChange={handleinput} />
                     <small></small>
+                    <label>Birthday:</label>
+                    <input type="date" id="birthday" name="employeeBirthday" value={employeeDetails.employeeBirthday} onChange={handleinput}></input>
                 </div>
                 <input ref={fileInputRef} type="file" name="filename" accept="image/x-png, image/gif, image/jpeg" value={employeeDetails.filename} onChange={handleinput} />
                 <input type="submit" value="submit" />
